f685d791d77c9a25a9a7b971eefcb1764c0fd961
Garpur3 <54945227+Garpur3@users.noreply.github.com>
2020-09-09 15:35:27 +0000

    Streamlined code and added comments

--- Percolation.java
+++ Percolation.java
@@ -5,27 +5,28 @@ import java.util.Iterator;
 
 public class Percolation {
 
-    private boolean[][] grid;
-
     private int gridSize;
+
+    private boolean[][] grid;
 
     private int openSites;
 
+    private boolean percolates;
+
     private WeightedQuickUnionUF path;
 
+    //Used when checking whether percolation is present
     private LinkedBag<Integer> topRow, botRow;
-
-    private boolean percolates;
 
     public Percolation(int n) {
         checkGrid(n);
         gridSize = n;
         grid = new boolean[n][n];
         openSites = 0;
-        path = new WeightedQuickUnionUF(n*n);
-        topRow = new LinkedBag<Integer>();
-        botRow = new LinkedBag<Integer>();
         percolates = false;
+        path = new WeightedQuickUnionUF(n*n);
+        topRow = new LinkedBag<>();
+        botRow = new LinkedBag<>();
     }
 
     private void checkGrid(int n) {
@@ -33,26 +34,23 @@ public class Percolation {
             throw new java.lang.IllegalArgumentException();
     }
 
-    private void checkSize(int row, int col) {
-        if(row < 0 || col < 0 || row > gridSize - 1 || col > gridSize - 1)
-            throw new java.lang.IndexOutOfBoundsException();
-    }
-
     public void open(int row, int col) {
-        checkSize(row, col);
+        checkSite(row, col);
 
+        //Only possible values for row and col are 0,0
         if(gridSize == 1) {
             grid[0][0] = true;
             percolates = true;
             openSites++;
         }
 
-        else if(grid[col][row] == false) {
+        else if(!isOpen(row, col)) {
             grid[col][row] = true;
             openSites++;
+
+            //Keeps track of open sites in top and bottom row
             if(row == 0)
                 topRow.add(col);
-
             else if(row == gridSize - 1)
                 botRow.add(col + gridSize*(gridSize - 1));
 
@@ -60,36 +58,66 @@ public class Percolation {
         }
     }
 
-    private void checkPercolation() {
-        if(topRow.size() > 0 && botRow.size() > 0) {
-            Iterator<Integer> top, bot;
-            top = topRow.iterator();
-            while(top.hasNext()) {
-                bot = botRow.iterator();
-                int tSite = top.next();
-                while(bot.hasNext()) {
-                    int bSite = bot.next();
-                    if(path.find(tSite) == path.find(bSite))
-                        percolates = true;
-                }
+    private void checkSite(int row, int col) {
+        if(!siteExists(row, col))
+            throw new java.lang.IndexOutOfBoundsException();
+    }
+
+    private boolean siteExists(int row, int col) {
+        if(row < 0 || row > gridSize - 1)
+            return false;
+        else if(col < 0 || col > gridSize - 1)
+            return false;
+        return true;
+    }
+
+    //Creates union for sites adjacent if they exist and are open
+    private void checkSurroundings(int row, int col){
+        int site = col + row*gridSize;
+
+        int[][] offset = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int newRow, newCol;
+
+        for(int i = 0; i < 4; i++) {
+            newRow = row + offset[i][0];
+            newCol = col + offset[i][1];
+
+            if(siteExists(newRow, newCol)) {
+                if(isOpen(newRow, newCol))
+                    path.union(newCol + newRow*gridSize, site);
             }
+        }
+
+        if(!percolates)
+            checkPercolation();
+    }
+
+    private void checkPercolation() {
+        Iterator<Integer> bot = botRow.iterator();
+        while(!percolates && bot.hasNext()) {
+            int bSite = bot.next();
+            int col = bSite % gridSize;
+            percolates = isFull(gridSize - 1, col);
         }
     }
 
     public boolean isOpen(int row, int col) {
-        checkSize(row, col);
+        checkSite(row, col);
         return grid[col][row];
     }
 
     public boolean isFull(int row, int col) {
-        checkSize(row, col);
-
         if(gridSize == 1)
             return isOpen(row, col);
 
-        int siteRoot = path.find(col + row*gridSize);
+        checkSite(row, col);
+
         if(topRow.size() > 0) {
+            int siteRoot = path.find(col + row*gridSize);
+
             Iterator<Integer> top = topRow.iterator();
+
             while(top.hasNext()) {
                 if(siteRoot == path.find(top.next()))
                     return true;
@@ -104,31 +132,6 @@ public class Percolation {
 
     public boolean percolates() {
         return percolates;
-    }
-
-    private void checkSurroundings(int row, int col){
-        int site = col + row*gridSize;
-        for(int i = -1; i < 2; i += 2) {
-            try {
-                if(grid[col][row + i] == true) {
-                    path.union(col + (row + i)*gridSize, site);
-                }
-            }
-            catch(IndexOutOfBoundsException e) {
-
-            }
-        }
-        for(int i = -1; i < 2; i += 2) {
-            try {
-                if(grid[col + i][row] == true) {
-                    path.union((col + i) + row*gridSize, site);
-                }
-            }
-            catch(IndexOutOfBoundsException e) {
-
-            }
-        }
-        checkPercolation();
     }
 
     public static void main(String[] args) {
